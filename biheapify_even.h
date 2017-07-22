456bb8b591424a3203c851030b926a0e75c8ce52
Matthew Gregory Krupa <mgkrupa@hotmail.com>
2017-07-22 13:27:08 -0700

    Update

--- biheapify_even.h
+++ biheapify_even.h
@@ -14,14 +14,16 @@
 
 #include "biheap_common.h"
 
-//Note that FlipCo(coord1) >= coord2 iff coord1 <= FlipCo(coord2) (ditto for >, <=, and <).
+/* Note that FlipCo(coord1) >= coord2 iff coord1 <= FlipCo(coord2)
+ * (ditto for >, <=, and <).
+ */
 #define FLIP_COORDINATE(a) (total_num_nodes - 1 - (a))
 
 /* Assumes that total_num_nodes is even and that the node pos_mc belongs to the
  *  min heap.
  */
 template<class RAI>
-void SiftFromMinToMaxEven(RAI first, size_type total_num_nodes,
+inline void SiftFromMinToMaxEven(RAI first, size_type total_num_nodes,
                           size_type num_nodes_in_heap,
                           size_type first_node_in_mirror_heap,
                           size_type pos_hc,
@@ -64,26 +66,27 @@ void SiftFromMinToMaxEven(RAI first, size_type total_num_nodes,
  *  max heap.
  */
 template<class RAI>
-void SiftFromMaxToMinEven(RAI first, size_type total_num_nodes,
+inline void SiftFromMaxToMinEven(RAI first, size_type total_num_nodes,
                           size_type num_nodes_in_heap,
                           size_type first_node_in_mirror_heap,
                           size_type pos_mc,
                           size_type smallest_node_in_biheap_hc) {
   auto pos_hc = FLIP_COORDINATE(pos_mc);
-  while (pos_mc < first_node_in_mirror_heap) { //While node pos_mc is NOT in the min heap
+  //While node pos_mc is NOT in the min heap.
+  while (pos_mc < first_node_in_mirror_heap) {
     if (pos_hc < smallest_node_in_biheap_hc) //If the node is not in the biheap
       return ;
 
     auto left_child_mc  = GetLeftChildInBiheap(pos_mc);
     auto right_child_mc = left_child_mc + 1; //= GetRightChildInBiheap(pos_mc);
-    auto left_child_hc  = FLIP_COORDINATE(left_child_mc); //= pos_hc - pos_mc - 1;
+    auto left_child_hc  = FLIP_COORDINATE(left_child_mc);
     auto right_child_hc = left_child_hc - 1; //= FLIP_COORDINATE(right_child_mc)
     auto pos_it   = first + pos_hc;
     auto left_it  = first + left_child_hc;
     auto right_it = first + right_child_hc;
 
-    bool is_right_child_valid = (right_child_hc >= smallest_node_in_biheap_hc) && //Is the node in the biheap?
-                                (right_child_mc < num_nodes_in_heap);             //Is the node in the max heap?
+    bool is_right_child_valid = (right_child_hc >= smallest_node_in_biheap_hc)  //Is the node in the biheap?
+                                && (right_child_mc < num_nodes_in_heap);        //Is the node in the max heap?
     //Note that the equivalent of is_right_child_valid for the left child, which is
     // (left_child <= largest_node_in_biheap_hc) && left_child  < num_nodes_in_heap)
     // is always true when total_num_nodes is even.
@@ -114,7 +117,7 @@ void SiftFromMaxToMinEven(RAI first, size_type total_num_nodes,
  * Experimentation (as opposed to proof) indicates that BiHeapifyEven() always
  *  succeeds in creating a biheap. Please contact the author if a
  *  counter-example is found in which case this algorithm can be made correct
- *  by mimicking the definitions of BiHeapifyOdd() and BiHeapifyOddSinglePass()
+ *  by mimicking the definitions of BiHeapifyOdd() and BiHeapifyOdd()
  *  found in biheapify_odd.h
  *
  * Remark:
@@ -127,42 +130,31 @@ void SiftFromMaxToMinEven(RAI first, size_type total_num_nodes,
  *  node in the max heap. The complexity of this biheapification is easily
  *  seen to be twice the complexity of the above mentioned heapification
  *  operation plus a constant.
- * (2) Unlike BiHeapifyOddSinglePass(), it appears that only one call to
- *  BiHeapifyEven() is needed in order to obtain a biheap.
+ * (2) It appears that only one call to BiHeapifyEven() is needed in order to
+ *  obtain a biheap.
  */
 template<class RAI>
-void BiHeapifyEvenSinglePass(RAI first, size_type total_num_nodes) {
-size_type biheap_lower_bound_node_hc = 0;
-size_type biheap_upper_bound_node_hc = 0;
-size_type node_to_start_biheapification_at = static_cast<size_type>(-1);
-  if (biheap_lower_bound_node_hc == 0 && biheap_upper_bound_node_hc == 0)
-    biheap_upper_bound_node_hc = total_num_nodes - 1;
-  if (total_num_nodes <= 0)
+void BiHeapifyEven(RAI first, size_type total_num_nodes) {
+  if (total_num_nodes <= 1)
     return ;
   auto num_nodes_in_heap = GetNumNodesInHeapContainedInBiheap(total_num_nodes);
   auto first_node_in_mirror_heap  = total_num_nodes - num_nodes_in_heap;
 
-  if (node_to_start_biheapification_at < biheap_lower_bound_node_hc
-   || node_to_start_biheapification_at > biheap_upper_bound_node_hc
-   || node_to_start_biheapification_at == static_cast<size_type>(-1)) {
-    auto num_nodes_to_biheapify = biheap_upper_bound_node_hc
-                                - biheap_lower_bound_node_hc + 1;
-    node_to_start_biheapification_at = biheap_lower_bound_node_hc
-        + (num_nodes_to_biheapify / 2) + (num_nodes_to_biheapify % 2);
-  }
-  size_type smallest_node_in_biheap_hc = node_to_start_biheapification_at;
-  size_type largest_node_in_biheap_hc  = node_to_start_biheapification_at - 1;
+  size_type smallest_node_in_biheap_hc = total_num_nodes / 2;
+  size_type largest_node_in_biheap_hc  = smallest_node_in_biheap_hc - 1;
 
   while(smallest_node_in_biheap_hc > 0) {
-    if (smallest_node_in_biheap_hc > biheap_lower_bound_node_hc) {
+    if (smallest_node_in_biheap_hc > 0) {
     --smallest_node_in_biheap_hc;
-    SiftFromMinToMaxEven<RAI>(first, total_num_nodes, num_nodes_in_heap, first_node_in_mirror_heap,
-                     smallest_node_in_biheap_hc, largest_node_in_biheap_hc);
+    SiftFromMinToMaxEven<RAI>(first, total_num_nodes, num_nodes_in_heap,
+                         first_node_in_mirror_heap, smallest_node_in_biheap_hc,
+                         largest_node_in_biheap_hc);
     }
-    if (largest_node_in_biheap_hc < biheap_upper_bound_node_hc) {
+    if (largest_node_in_biheap_hc < total_num_nodes - 1) {
       ++largest_node_in_biheap_hc;
-      SiftFromMaxToMinEven<RAI>(first, total_num_nodes, num_nodes_in_heap, first_node_in_mirror_heap,
-                       FLIP_COORDINATE(largest_node_in_biheap_hc), smallest_node_in_biheap_hc);
+      SiftFromMaxToMinEven<RAI>(first, total_num_nodes, num_nodes_in_heap,
+          first_node_in_mirror_heap, FLIP_COORDINATE(largest_node_in_biheap_hc),
+          smallest_node_in_biheap_hc);
     }
   }
   return ;
@@ -184,7 +176,7 @@ size_type node_to_start_biheapification_at = static_cast<size_type>(-1);
  * Experimentation (as opposed to proof) indicates that BiHeapifyEven() always
  *  succeeds in creating a biheap. Please contact the author if a
  *  counter-example is found in which case this algorithm can be made correct
- *  by mimicking the definitions of BiHeapifyOdd() and BiHeapifyOddSinglePass()
+ *  by mimicking the definitions of BiHeapifyOdd() and BiHeapifyOdd()
  *  found in biheapify_odd.h
  *
  * Remark:
@@ -197,11 +189,11 @@ size_type node_to_start_biheapification_at = static_cast<size_type>(-1);
  *  node in the max heap. The complexity of this biheapification is easily
  *  seen to be twice the complexity of the above mentioned heapification
  *  operation plus a constant.
- * (2) Unlike BiHeapifyOddSinglePass(), it appears that only one call to
- *  BiHeapifyEven() is needed in order to obtain a biheap.
+ * (2) It appears that only one call to BiHeapifyEven() is needed in order to
+ *  obtain a biheap.
  */
 template<class RAI>
-void BiHeapifyEvenSinglePass(RAI first, size_type total_num_nodes,
+void BiHeapifyEven(RAI first, size_type total_num_nodes,
                       size_type biheap_lower_bound_node_hc,
                       size_type biheap_upper_bound_node_hc = 0,
                       size_type node_to_start_biheapification_at = static_cast<size_type>(-1)) {
@@ -226,22 +218,55 @@ void BiHeapifyEvenSinglePass(RAI first, size_type total_num_nodes,
   while(smallest_node_in_biheap_hc > 0) {
     if (smallest_node_in_biheap_hc > biheap_lower_bound_node_hc) {
     --smallest_node_in_biheap_hc;
-    SiftFromMinToMaxEven<RAI>(first, total_num_nodes, num_nodes_in_heap, first_node_in_mirror_heap,
-                     smallest_node_in_biheap_hc, largest_node_in_biheap_hc);
+    SiftFromMinToMaxEven<RAI>(first, total_num_nodes, num_nodes_in_heap,
+                           first_node_in_mirror_heap,smallest_node_in_biheap_hc,
+                           largest_node_in_biheap_hc);
     }
     if (largest_node_in_biheap_hc < biheap_upper_bound_node_hc) {
       ++largest_node_in_biheap_hc;
-      SiftFromMaxToMinEven<RAI>(first, total_num_nodes, num_nodes_in_heap, first_node_in_mirror_heap,
-                       FLIP_COORDINATE(largest_node_in_biheap_hc), smallest_node_in_biheap_hc);
+      SiftFromMaxToMinEven<RAI>(first, total_num_nodes, num_nodes_in_heap,
+         first_node_in_mirror_heap, FLIP_COORDINATE(largest_node_in_biheap_hc),
+         smallest_node_in_biheap_hc);
     }
   }
   return ;
 }
 
+/*This will BiHeapify all nodes in [0, total_num_nodes).
+ * Assumes that total_num_nodes is even.
+ */
+template<class RAI>
+inline void BiHeapifySafeEven(RAI first, size_type total_num_nodes) {
+  //If it's small enough that it's easiest to just sort everything.
+  if (total_num_nodes < 2)
+    return ;
+
+  /* Remarks:
+   * (1) The following do {} while(); loop will stop due to the same reasoning
+   *  that can be found in Remark (1) in BiHeapifyOdd()'s definition.
+   *
+   * (2) Experimentation shows that the only one call to
+   *  BiHeapifyEven() is ever needed to form a biheap.
+   */
+  BiHeapifyEven(first, total_num_nodes);
+
+  /* The while loop below should be redundant and is included just in case
+   *  BiHeapifyEven() didn't successfully form a biheap. This is
+   *  recommended if performance is important since IsBiheap() is an
+   *  O(total_num_nodes) operation.
+   */
+  while(!IsBiheap(first, total_num_nodes)) {
+    //The line below can be removed without affecting the correctness of the
+    // algorithm.
+    GetBiHeapifyFailureMessage(first, total_num_nodes);
+    BiHeapifyEven(first, total_num_nodes);
+  }
+  return ;
+}
 
 /*This will BiHeapify all nodes in
  * [biheap_lower_bound_node_hc, biheap_upper_bound_node_hc]
- *  (including these endpoints).
+ *  (including these end points).
  * If biheap_start_node_hc == biheap_end_node_hc == 0 then biheap_end_node_hc
  *  will be replaced by total_num_nodes - 1.
  * If node_to_start_biheapification_at == static_cast<size_type>(-1) or is
@@ -249,22 +274,20 @@ void BiHeapifyEvenSinglePass(RAI first, size_type total_num_nodes,
  *  [biheap_lower_bound_node_hc, biheap_upper_bound_node_hc]
  *  then node_to_start_biheapification_at will be set to the midpoint of the
  *  interval [biheap_lower_bound_node_hc, biheap_upper_bound_node_hc], rounded
- *  up (i.e. itwill then be increased by 1 if the number of nodes in this
+ *  up (i.e. it will then be increased by 1 if the number of nodes in this
  *  interval is odd.)
  * Assumes that total_num_nodes is odd.
 */
 template<class RAI>
-void BiHeapifyEven(RAI first, size_type total_num_nodes,
-                      size_type biheap_lower_bound_node_hc = 0,
+void BiHeapifySafeEven(RAI first, size_type total_num_nodes,
+                      size_type biheap_lower_bound_node_hc,
                       size_type biheap_upper_bound_node_hc = 0,
                       size_type node_to_start_biheapification_at = static_cast<size_type>(-1)) {
   if (biheap_lower_bound_node_hc == 0 && biheap_upper_bound_node_hc == 0)
     biheap_upper_bound_node_hc = total_num_nodes - 1;
   //If it's small enough that it's easiest to just sort everything.
-  if(biheap_upper_bound_node_hc - biheap_lower_bound_node_hc < 2) {
-    std::sort(first + biheap_lower_bound_node_hc, first + (biheap_upper_bound_node_hc + 1));
+  if (biheap_upper_bound_node_hc - biheap_lower_bound_node_hc < 1)
     return ;
-  }
 
   if (node_to_start_biheapification_at < biheap_lower_bound_node_hc
    || node_to_start_biheapification_at > biheap_upper_bound_node_hc
@@ -279,23 +302,22 @@ void BiHeapifyEven(RAI first, size_type total_num_nodes,
    * (1) The following do {} while(); loop will stop due to the same reasoning
    *  that can be found in Remark (1) in BiHeapifyOdd()'s definition.
    *
-   * (2) Experimentation shows that the only one call to
-   *  BiHeapifyEvenSinglePass() is ever needed to form a biheap in this case
-   *  where total_num_nodes is even.
+   * (2) Experimentation shows that the only one call to BiHeapifyEven()
+   *  is ever needed.
    */
-  BiHeapifyEvenSinglePass(first, total_num_nodes, biheap_lower_bound_node_hc,
+  BiHeapifyEven(first, total_num_nodes, biheap_lower_bound_node_hc,
                   biheap_upper_bound_node_hc, node_to_start_biheapification_at);
 
   /* The while loop below should be redundant and is included just in case
-   *  BiHeapifyEvenSinglePass() didn't successfully form a biheap. This is
+   *  BiHeapifyEven() didn't successfully form a biheap. This is
    *  recommended if performance is important since IsBiheap() is an
    *  O(total_num_nodes) operation.
    */
   while(!IsBiheap(first, total_num_nodes, 0, total_num_nodes - 1, false)) {
-    //The line below can be removed without affecting the correctness of the
-    // algorithm.
+    //The call to GetBiHeapifyFailureMessage() below can be removed without
+    // affecting the correctness of the algorithm.
     GetBiHeapifyFailureMessage(first, total_num_nodes);
-    BiHeapifyEvenSinglePass(first, total_num_nodes, biheap_lower_bound_node_hc,
+    BiHeapifyEven(first, total_num_nodes, biheap_lower_bound_node_hc,
                   biheap_upper_bound_node_hc, node_to_start_biheapification_at);
   }
   return ;
