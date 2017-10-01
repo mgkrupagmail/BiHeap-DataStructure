c4e343f9b09e5ba2c8f2f8a3751e8461834c65c3
Matthew Gregory Krupa <mgkrupa@hotmail.com>
2017-10-01 16:45:53 -0700

    Update

--- biheap_sift_test_correctness.h
+++ biheap_sift_test_correctness.h
@@ -2,7 +2,7 @@
  * biheap_sift_test_correctness.h
  *
  *  Created on: Jul 25, 2017
- *      Author: diagoras
+ *      Author: Matthew Gregory Krupa
  */
 
 #ifndef BIHEAP_SIFT_TEST_CORRECTNESS_H_
@@ -10,36 +10,35 @@
 
 #include <random>
 #include "random_helpers.h"
+#include "biheap_common.h"
 #include "biheapify.h"
 #include "biheap_sift.h"
 
-template<class T>
+template<class T, typename size_type = std::size_t>
 bool BiHeapSiftTestCorrectness(long start_total_num_nodes,
                                long end_total_num_nodes,
                                long num_vecs_to_try = 1,
                                long increment_size = 1) {
   int total_num_nodes = start_total_num_nodes;
+  std::random_device rnd_device;
+  std::mt19937 generator(rnd_device());
   while (total_num_nodes <= end_total_num_nodes) {
     std::vector<T> vec(total_num_nodes + 1);
+    std::uniform_int_distribution<T> dist(0, total_num_nodes - 1);
     for (auto vec_counter = 0l; vec_counter < num_vecs_to_try; vec_counter++) {
       randomhelpers::FillWithRandomNumbers(vec.begin(), vec.end(),
                 0, static_cast<T>(4*total_num_nodes));//std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
       size_type pos_hc;
-      {
-        std::random_device rnd_device;
-        std::mt19937 generator(rnd_device());
-        std::uniform_int_distribution<T> dist(0, total_num_nodes - 1);
-        pos_hc = dist(generator);
-      }
+      pos_hc = dist(generator);
       //Form a biheap.
-      BiHeapify(vec.begin(), total_num_nodes);
+      BiHeapify<typename std::vector<T>::iterator, size_type>(vec.begin(), total_num_nodes);
       //Change one randomly selected element to some random value (which was
       // stored outside of the biheap in vec[total_num_nodes]).
       vec[pos_hc] = vec[total_num_nodes];
       //Sift the changed element.
-      BiHeapSift(vec.begin(), total_num_nodes, pos_hc);
+      BiHeapSift<typename std::vector<T>::iterator, size_type>(vec.begin(), total_num_nodes, pos_hc);
       //Check that we once again have a biheap.
-      bool is_biheap = IsBiheap(vec.begin(), total_num_nodes);
+      bool is_biheap = IsBiheap<typename std::vector<T>::iterator, size_type>(vec.begin(), total_num_nodes);
       if (!is_biheap) {
         std::cout << "Sift() failed to form a biheap. "
                   << "total_num_nodes = " << total_num_nodes << std::endl;
