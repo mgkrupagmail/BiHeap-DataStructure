0f0a2139e31ddf7e7c83ef720cd9bc203ef6be34
Matthew Gregory Krupa <mgkrupa@hotmail.com>
2017-11-09 10:15:45 -0800

    Update

--- biheapify_time.h
+++ biheapify_time.h
@@ -3,6 +3,7 @@
  *
  *  Created on: Jun 27, 2017
  *      Author: Matthew Gregory Krupa
+ *   Copyright: Copyright Matthew Gregory Krupa
  *
  *  The TimeBiHeapifies() functions times the various BiHeapify functions.
  *  An example call can be found in main().
@@ -19,10 +20,47 @@
 #include <random>
 #include <vector>
 
-#include "biheap_common.h"
+#include "../../CppBiHeapMedian/src/biheapify_simple.h"
 #include "biheapify.h"
-#include "biheapify_simple.h"
+#include "biheapify_single_pass_success_rate.h"
 #include "random_helpers.h"
+/* Similar to BiHeapifyOdd(), except that it biheapifies an odd collection of
+ *  elements using a pair of calls to BiHeapifyEven() in lieu of the
+ *  single call to BiHeapifyOdd() that's done in BiHeapifyOdd().
+ * It is not clear why this function empirically succeeds in biheapifying
+ *  an odd number of elements.
+ */
+/* REMARK:
+ * (1) If you were to replace the two calls to BiHeapifyEven() with
+ *      two calls to BiHeapifyOdd(), then there is input data that the
+ *      resulting function will NEVER succeed in biheapifying
+ *      no matter how many times it repeatedly calls these two functions.
+ *     This starts to happen when total_num_nodes >= 11.
+ *     It is not clear why BiHeapifyUsingBiHeapifyEven() empirically always
+ *      appears to produce a biheap while this fails to be true if
+ *      BiHeapifyEven() is replaced with BiHeapifyOdd().
+ */
+template<class RAI, typename size_type = std::size_t>
+inline void BiHeapifyUsingBiHeapifyEven(RAI first, size_type total_num_nodes) {
+  if (total_num_nodes < 3) {
+    if (total_num_nodes == 2 && *first > *(first + 1))
+      std::iter_swap(first, first + 1);
+    return ;
+  }
+
+  auto counter = 0u;
+  do {
+    BiHeapify<RAI, size_type>(first + 1, total_num_nodes - 1);
+    BiHeapify<RAI, size_type>(first, total_num_nodes - 1);
+    if (counter != 0 && counter % 3 == 0) {
+      std::cerr << "BiHeapifyUsingBiHeapifyEven() could not biheapify after "
+                << (counter + 1) << " call(s)." << std::endl;
+      assert(false);
+    }
+    counter++;
+  } while(!IsBiHeap<RAI, size_type>(first, total_num_nodes));
+  return ;
+}
 
 template<class T, typename size_type = std::size_t>
 std::chrono::nanoseconds TimeBiHeapifyGivenVec(T &vec,
@@ -68,8 +106,8 @@ std::chrono::nanoseconds::rep TimeBiHeapifyOnGivenSize(
     randomhelpers::FillWithRandomNumbers(vec.begin(), vec.end(),
                   std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
     vec_original = vec;
-    total += TimeBiHeapifyGivenVec<std::vector<T>, size_type>(vec, vec_original, total_num_nodes,
-                                             num_repititions_per_vec);
+    total += TimeBiHeapifyGivenVec<std::vector<T>, size_type>(vec, vec_original,
+                                      total_num_nodes, num_repititions_per_vec);
   }
   return total.count();
 }
@@ -84,30 +122,29 @@ std::chrono::nanoseconds::rep TimeBiHeapifyUsingBiHeapifyEvenOnGivenSize(
     randomhelpers::FillWithRandomNumbers(vec.begin(), vec.end(),
                   std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
     vec_original = vec;
-    total += TimeBiHeapifyUsingBiHeapifyEvenGivenVec<std::vector<T>, size_type>(vec, vec_original,
-                                      total_num_nodes, num_repititions_per_vec);
+    total += TimeBiHeapifyUsingBiHeapifyEvenGivenVec<std::vector<T>, size_type>(vec,
+                            vec_original, total_num_nodes, num_repititions_per_vec);
   }
   return total.count();
 }
 
 std::string GetTimeString(std::chrono::nanoseconds::rep time_rep, long long divisor) {
   std::stringstream strm;
-  strm << time_rep/divisor << " ns  = "
-       << (time_rep/divisor)/1000 << " mus  = "
+  strm << time_rep/divisor << " ns  = " << (time_rep/divisor)/1000 << " mus  = "
        << (time_rep/divisor)/1000000 << " ms ";
   return strm.str();
 }
 
-template<class T, typename size_type = std::size_t> void TimeBiHeapifies(long start_total_num_nodes,
-                                       long end_total_num_nodes,
-                                       long num_vecs_to_try = 1,
-                                       long num_repititions_per_vec = 1,
-                                       long increment_size = 1,
-                                       long long divisor = 0,
-                                       bool should_time_BiHeapify = true,
-                                       bool should_time_BiHeapifySafe = true,
-                                       bool should_time_BiHeapifyUsingEven =
-                                                                         true) {
+template<class T, typename size_type = std::size_t>
+void TimeBiHeapifies(long start_total_num_nodes,
+                     long end_total_num_nodes,
+                     long num_vecs_to_try = 1,
+                     long num_repititions_per_vec = 1,
+                     long increment_size = 1,
+                     long long divisor = 0,
+                     bool should_time_BiHeapify = true,
+                     bool should_time_BiHeapifySafe = true,
+                     bool should_time_BiHeapifyUsingEven = true) {
   if (divisor == 0)
     divisor = num_vecs_to_try * num_repititions_per_vec;
   else if (divisor < 0)
